693afb0cf38c34e458a140f88dc18494492f380f
aditya Singh <singhap_2@rknec.edu>
2025-06-06 01:35:02 +0530

    scripts

---
+++ scripts/postgre.sh
@@ -0,0 +1,1 @@
+docker run --name=postgre -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=bv -p 5432:5432 -d postgres
---
+++ scripts/test_db.sh
@@ -0,0 +1,1 @@
+docker run --name=test_postgre -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=bv -p 1234:5432 -d postgres
