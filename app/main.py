fbee89c3f41563cadd07a2403b8bbdf09357c90f
aditya Singh <singhap_2@rknec.edu>
2025-06-11 18:28:31 +0530

    refactored main.py

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from db import Base, engine
+from contextlib import asynccontextmanager
+import views
+
+
+# lifespan
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(views.router)
