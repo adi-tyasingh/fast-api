c5775ada9c6460c4052393727081f1b42c53429d
aditya Singh <singhap_2@rknec.edu>
2025-06-11 18:28:48 +0530

    refactored db.py

---
+++ app/db.py
@@ -0,0 +1,44 @@
+from typing import Optional
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, URL
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv("../.env")
+
+url_object = URL.create(
+    "postgresql+asyncpg",
+
+
+
+    
+    username=os.getenv("USERNAME"),
+    password=os.getenv("PASSWORD"),
+    host=os.getenv("HOST"),
+    port=int(os.getenv("PORT")),  # Convert port to integer
+    database=os.getenv("DB"),
+)
+
+# port = os.getenv("PORT")
+# db = os.getenv("DB")
+# host = os.getenv("HOST")
+
+# db_string = f"postgresql+asyncpg://admin:pass@localhost:{port}/{db}"
+
+
+engine = create_async_engine(url_object, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class Book(Base):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    author: Mapped[str] = mapped_column(String(100))
+    description: Mapped[Optional[str]]
